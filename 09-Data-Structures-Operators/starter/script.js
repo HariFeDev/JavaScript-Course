47ff28eabac1aa30cc4e37b8f428e4b9e4fb6813
Hari <harifedev@gmail.com>
2025-02-03 23:14:49 +0530

    Started Data Structures and Operators 🎉

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,19 @@ const restaurant = {
     },
   },
 };
+
+// ----------- Destructing Arrays ------------
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a, b, c);
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const [first, , second] = restaurant.categories;
+console.log(restaurant.categories);
+console.log(first, second);
