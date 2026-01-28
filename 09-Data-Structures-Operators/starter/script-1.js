8860b9338bb68c88979d7dffdf22c9a3e05a561d
HARI_WIN <harifedev@gmail.com>
2026-01-28 22:37:35 +0530

    Learning Maps and Challenges

--- 09-Data-Structures-Operators/starter/script-1.js
+++ 09-Data-Structures-Operators/starter/script-1.js
@@ -95,3 +95,103 @@ const getCommonSkills = function () {
 }
 
 console.log(getCommonSkills());
+
+// Maps 
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano')
+rest.set(1, 'Frizene Italy')
+console.log(rest.set(2, 'Lisbon Portugal'));
+
+rest.set('Categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']).set('open', 11).set('close', 23).set(true, "We are Opening 🤩").set(false, "We are Closed 😔");
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 1;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('Categories'));
+rest.delete(2);
+// rest.clear()
+const arr = [1, 2];
+rest.set(arr, 'Test')
+rest.set(document.querySelector('h1'), "Heading")
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+// Maps Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+])
+console.log(question);
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[6]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+console.log(Object.entries(openingHours));
+
+// Convert Object to Map
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz App
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.entries()]);
+console.log([...question.values()]);
+console.log([...question.keys()]);
+
+// Maps Challenge
+const fruits = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple'];
+const fruitsMap = new Map();
+// const fruitsSet = new Set(fruits)
+// console.log(fruitsSet);
+let count = 0;
+let currFruit;
+for (const fruit of fruits) {
+  console.log(fruit);
+  currFruit = fruit
+  // console.log(currFruit);  
+  for (const repFruit of fruits) {
+    if (repFruit === currFruit) {
+      count += 1;
+      console.log(`repFruit: ${repFruit} and currFruit: ${currFruit} - ${count}`);
+    }
+  }
+}
