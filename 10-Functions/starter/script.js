4c1c08726370bb4a881d2b5d7bcac43424696206
HARI_WIN <harifedev@gmail.com>
2026-02-04 07:06:03 +0530

    Started Closer Look at Functions - Values VS Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+// ----------- Default Parameters
+/*
+const bookings = [];
+
+const createBooking = function (flightNum, numPassenger = 1, price = 199 * numPassenger) {
+
+  // Before the ES6 - old way of doing it
+  // numPassenger = numPassenger || 1;
+  // price = price || 199
+
+  const booking = {
+    flightNum,
+    numPassenger,
+    price
+  }
+  console.log(booking);
+  bookings.push(booking);
+}
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+// To skip the parameter set that to undefined
+createBooking('LH123', undefined, 15);
+*/
+
+// ----------- Values VS Reference
+const flight = 'LH123';
+const jonas = {
+  name: 'Jonas schedtmann',
+  passport: 2321454580,
+}
