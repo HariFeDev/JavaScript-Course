a095ab653cdee58eb3f2d9cb0a1efdc0f5aaa6db
HARI_WIN <harifedev@gmail.com>
2026-07-13 22:26:03 +0530

    Finsihed Working with Arrays section 🐸

--- 11-Arrays-Bankist/starter/challenge.js
+++ 11-Arrays-Bankist/starter/challenge.js
@@ -62,6 +62,7 @@ const avg4 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg3, avg4);
 
 // coding challenge #4
+/*
 const dogs = [
   {
     weight: 22,
@@ -87,7 +88,7 @@ const dogs = [
 
 /*
 
-*/
+
 
 // 1
 dogs.map((dog) => dog.recFood = Math.round(dog.weight ** 0.75 * 28));
@@ -138,3 +139,106 @@ console.log(okaydogs);
 // 8
 const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
 console.log(dogsSorted);
+
+*/
+
+// Coding Challenge #5
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John', 'Leo'] },
+  { weight: 18, curFood: 244, owners: ['Joe'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1
+console.log(dogs);
+
+dogs.forEach((dog, i, arr) => {
+  dog.recFood = Math.round(dog.weight ** 0.75 * 28);
+})
+
+console.log(dogs);
+
+// 2
+dogs.find((dog) => {
+  if (dog.owners.includes('Sarah')) {
+    console.log(dog);
+    if (dog.curFood > (dog.recFood * 0.90) && dog.curFood < (dog.recFood * 1.10)) {
+      console.log('Okay amount of food');
+    }
+
+    else if (dog.curFood > dog.recFood) {
+      console.log('Larger amount of food');
+    }
+
+    else if (dog.curFood < dog.recFood) {
+      console.log('Little amount of food');
+    }
+  }
+})
+
+// 3
+
+const ownersTooMuch = dogs.filter((dog) => {
+  if (dog.curFood > dog.recFood) {
+    return dog;
+  }
+}).flatMap((owner) => owner.owners)
+
+console.log(ownersTooMuch);
+
+const ownersTooLittle = dogs.filter((dog) => {
+  if (dog.curFood < dog.recFood) {
+    return dog;
+  }
+}).flatMap((owner) => owner.owners);
+
+console.log(ownersTooLittle);
+
+// 4
+console.log(`${ownersTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersTooLittle.join(' and ')}'s dogs eat too little!`);
+// "Matilda and Alice and Bob's dogs eat too much!"
+
+// 5
+console.log(dogs.some((dog) => dog.curFood === dog.recFood));
+
+// 6
+const isEatingOkay = (dog) => {
+  return dog.curFood > (dog.recFood * 0.90) && dog.curFood < (dog.recFood * 1.10)
+}
+
+console.log(dogs.every(isEatingOkay));
+
+// 7
+const isEatingOkayArr = dogs.filter(isEatingOkay);
+console.log(isEatingOkayArr);
+
+// 8
+
+const groupByFood = Object.groupBy(dogs, (dog) => {
+  if (dog.curFood > (dog.recFood * 0.90) && dog.curFood < (dog.recFood * 1.10)) {
+    return 'okayAmount';
+  }
+
+  else if (dog.curFood > dog.recFood) {
+    return 'tooMuch';
+  }
+  else if (dog.curFood < dog.recFood) {
+    return 'tooLittle';
+  }
+});
+console.log(groupByFood);
+
+// 9
+
+const groupbyOwners = Object.groupBy(dogs, (dog) => {
+  return dog.owners.length;
+})
+
+console.log(groupbyOwners);
+
+// 10
+const sortByRecFood = dogs.toSorted((a, b) => a.recFood - b.recFood);
+console.log(sortByRecFood);
