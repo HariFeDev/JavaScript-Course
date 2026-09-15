991f5c3bb5b9522806e180c667e725194bd4bae4
HARI_WIN <harifedev@gmail.com>
2026-09-15 10:34:00 +0530

    Operations with Dates Finished

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -20,10 +20,10 @@ const account1 = {
     '2019-12-23T07:42:02.383Z',
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
+    '2026-09-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2026-09-12T18:49:59.371Z',
+    '2026-09-10T12:01:20.894Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -42,8 +42,8 @@ const account2 = {
     '2020-01-25T14:18:46.235Z',
     '2020-02-05T16:33:06.386Z',
     '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2026-09-12T18:49:59.371Z',
+    '2026-09-10T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -81,36 +81,55 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (acc, sort = false) {
-  containerMovements.innerHTML = '';
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
-  const combineMovsDates = acc.movements.map((mov, i) => ({ movement: mov, movementDate: acc.movementsDates.at(i) }));
-  console.log(combineMovsDates);
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
 
-  if (sort) {
-    combineMovsDates.sort((a, b) => a.movement - b.movement)
+  if (daysPassed === 0) {
+    return 'Today'
   }
-
-  combineMovsDates.forEach(function (obj, i) {
-    const { movement, movementDate } = obj;
-    const type = movement > 0 ? 'deposit' : 'withdrawal';
-
-    const date = new Date(movementDate);
-
+  if (daysPassed === 1) {
+    return 'Yesterday'
+  }
+  if (daysPassed <= 7) {
+    return `${daysPassed} days ago`
+  }
+  else {
     const day = `${date.getDate()}`.padStart(2, 0);
     const month = `${date.getMonth() + 1}`.padStart(2, 0);
     const year = date.getFullYear();
+    return `${day}/${month}/${year}`
+  }
+}
 
-    const displayDate = `${day}/${month}/${year}`
+const displayMovements = function (acc, sort = false) {
+  containerMovements.innerHTML = '';
 
-    const calcDaysPassed = (date1, date2) => Math.abs((date2 - date1)) / (1000 * 60 * 60 * 24);
+  // const combineMovsDates = acc.movements.map((mov, i) => ({ movement: mov, movementDate: acc.movementsDates.at(i) }));
+  // console.log(combineMovsDates);
+
+  // if (sort) {
+  //   combineMovsDates.sort((a, b) => a.movement - b.movement)
+  // }
+
+  const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
+
+  movs.forEach(function (mov, i) {
+    // const { movement, movementDate } = obj;
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i]);
+    console.log(acc.movementsDates[i]);
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1
       } ${type}</div>
         <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${movement.toFixed(2)}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
