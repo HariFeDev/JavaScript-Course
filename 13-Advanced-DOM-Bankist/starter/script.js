d572c7af92bf2eabd7006b1058ad213ceb40ac49
HARI_WIN <harifedev@gmail.com>
2026-09-26 11:47:37 +0530

    Types of Event and Event handlers finished

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,15 +30,49 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.scrollX, Window.scrollY);
+
+  console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // Scrolling
+  // Old school technique
+
+  // viewport distance + window distance
+  // window.scrollTo(s1coords.left + Window.scrollX, s1coords.top + window.scrollY)
+
+  // Smooth scroll
+  // window.scrollTo({
+  //   left: s1coords.left + Window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth'
+  // })
+
+  // New Technique
+  section1.scrollIntoView({
+    behavior: 'smooth'
+  })
+
+})
+
 // ---------- Lectures ------------
 console.log('---------- Lectures ------------');
 
 // Selecting Elements
+/*
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section')
 console.log(allSections);
 
@@ -52,4 +86,83 @@ console.log(document.getElementsByClassName('btn'));
 // Creating and inserting elements
 const message = document.createElement('div');
 message.classList.add('cookie-message');
-message.textContent = 'We use cookied for improved functionality and analytics.'
+// message.textContent = 'We use cookied for improved functionality and analytics.'
+message.innerHTML = 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'
+
+// header.prepend(message);
+header.append(message)
+// header.append(message.cloneNode(true))
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function () {
+  // message.parentElement.removeChild(message);
+  message.remove()
+})
+
+// Styles
+message.style.backgroundColor = '#37383d'
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+console.log(logo.classList);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attribute
+console.log(logo.dataset.versionNumber);
+
+// Classes 
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Don't use this
+logo.className = 'jonas'
+
+*/
+
+// Events
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D')
+}
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => {
+  h1.removeEventListener('mouseenter', alertH1);
+}, 3000)
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D')
+// }
